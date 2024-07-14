d8037ee4c9fa38156c7943a692fce0b565d4c2f0
Anushka Singh <anushkasingh272005@gmail.com>
2024-07-14 12:32:45 +0530

    Update guessing game.cpp

--- guessing game.cpp
+++ guessing game.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
+using namespace std;
+
+int main() {
+    srand(time(0)); // seed the random number generator
+    int secretNumber = rand() % 100 + 1; // generate a random number between 1 and 100
+    int guess;
+
+    cout << "Welcome to the guessing game!" << endl;
+    cout << "I'm thinking of a number between 1 and 100." << endl;
+
+    while (true) {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess == secretNumber) {
+            cout << " Congratulations! You guessed it!" << endl;
+            break;
+        } else if (guess < secretNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Too high! Try again." << endl;
+        }
+    }
+
+    return 0;
+}
